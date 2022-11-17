5c9a372d5a813a0aa644f39a87b4e1325cf7c4b8
Francisco PÉREZ <francisco.perez@solera.com>
2022-11-17 12:34:34 +0100

    modal added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import React,{ useEffect, useState } from "react";
 import "./App.css";
 import UsersTable from "./components/UsersTable";
+import Modal from 'react-bootstrap/Modal';
 
 function App() {
   const [userInfo, setUserInfo] = useState();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     fetch("http://localhost:8080/api/v1/get-all-users")
@@ -16,8 +21,19 @@ function App() {
 
   return (
     <div className="App">
-      <button className="button-3">Add new User</button>
+      <button className="button-3" variant="primary" onClick={handleShow}>Add new User</button>
       {userInfo && <UsersTable userInformation={userInfo}></UsersTable>}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+         
+        </Modal.Footer>
+      </Modal>
+     
       
     </div>
   );
